3963d59ee460bb5534c71339cab6799197bd46e9
Jingqing  Zhang <jingqing.zhang15@imperial.ac.uk>
2019-03-07 14:00:16 +0000

    initial commit

---
+++ src/dataloader.py
@@ -0,0 +1,187 @@
+# Dataloader
+# Data loading and early analysis on data
+
+import os
+import json
+import pronto
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# environment variables, subject to changes according to local environment
+# the folder that contains data
+data_dir = "../../data/MedicalText/"
+# the path to HPO ontology file
+hpo_ontology_file = data_dir + "HPO/hp.owl"
+hpo_description_file = data_dir + "HPO/description.json"
+# the path to MIMIC-III files
+mimic_note_file = data_dir + "MIMIC/data/NOTEEVENTS.csv"
+mimic_diag_file = data_dir + "MIMIC/data/DIAGNOSES_ICD.csv"
+mimic_file = data_dir + "MIMIC/data/ready.csv"
+
+def load_hpo_ontology():
+    ont = pronto.Ontology(hpo_ontology_file)
+    data = json.loads(ont.json)
+    return data
+
+def load_hpo_description():
+    import queue
+
+    if not os.path.exists(hpo_description_file):
+
+        print("Loading HPO description ...")
+
+        domains = [
+            'name',
+            'desc',
+            'other/comment',
+            'other/hasExactSynonym',
+            'other/hasRelatedSynonym'
+        ]
+
+        data = load_hpo_ontology()
+        process_data = dict()
+        start_id = "HP:0000001"
+
+        node_queue = queue.Queue()
+        node_queue.put(start_id)
+
+        while not node_queue.empty():
+            node = node_queue.get()
+            process_data[node] = dict()
+            for d in domains:
+                if "/" in d:
+                    d1 = d.split("/")[0]
+                    d2 = d.split("/")[1]
+                    text = ", ".join(data[node].get(d1, {}).get(d2, []))
+                else:
+                    text = data[node].get(d, "")
+            process_data[node][d] = text
+
+            children = data[node]["relations"].get("can_be", [])
+            for cnode in children:
+                node_queue.put(cnode)
+
+        with open(hpo_description_file, 'w') as outfile:
+            json.dump(data, outfile)
+
+        print("HPO descriptions have been extracted and save to %s" % hpo_description_file)
+
+    else:
+
+        print("Loading HPO descriptions from file %s" % hpo_description_file)
+
+        with open(hpo_description_file, 'r') as infile:
+            data = json.load(infile)
+
+    return data
+
+def plot_hpo_ontology():
+
+    data = load_hpo_ontology()
+
+    from pyvis.network import Network
+    import queue
+    net = Network()
+
+    # HP:0000001 All
+    # HP:0000018 Phenotypic Abnormality
+    # Other branch is also necessary
+    start_id = "HP:0000001"
+
+    node_queue = queue.Queue()
+    node_queue.put(start_id)
+    net.add_node(start_id)
+
+    node_used = 1
+
+    while not node_queue.empty():
+        node = node_queue.get()
+        children = data[node]["relations"].get("can_be", [])
+        for cnode in children:
+            node_queue.put(cnode)
+            net.add_node(cnode)
+            net.add_edge(node, cnode)
+            node_used += 1
+        if node_used > 300:
+            break
+
+    net.show("hpo.html")
+
+def load_mimic():
+
+    if not os.path.exists(mimic_file):
+
+        def _merge_note(df):
+            text = ""
+            for idx, row in df.iterrows():
+                text += row["TEXT"]
+            ndf = pd.DataFrame(
+                data={
+                    "HADM_ID":[df["HADM_ID"].iloc[0]],
+                    "SUBJECT_ID": [df["SUBJECT_ID"].iloc[0]],
+                    "TEXT": text
+                }
+            )
+            return ndf
+
+        print("Loading and processing MIMIC note data ...")
+
+        note_data = pd.read_csv(mimic_note_file)
+        note_data = note_data[note_data["CATEGORY"].str.contains("Discharge summary")]
+        note_data = note_data[["SUBJECT_ID", "HADM_ID", "TEXT"]]
+        note_data["SUBJECT_ID"] = note_data["SUBJECT_ID"].astype(str, copy=False)
+        note_data["HADM_ID"] = note_data["HADM_ID"].astype(str, copy=False)
+        note_data = note_data.groupby("HADM_ID").apply(_merge_note).reset_index(drop=True)
+
+        assert not any(note_data["HADM_ID"].duplicated())
+        note_data = note_data.set_index("HADM_ID")
+
+        note_data.to_csv(mimic_note_file + "_processed.csv")
+
+        def _merge_icd(df):
+            max_seq = df["SEQ_NUM"].iloc[-1]
+            for idx, row in df.iterrows():
+                max_seq = max(row["SEQ_NUM"], max_seq)
+            icd_code_list = [None] * max_seq
+            for idx, row in df.iterrows():
+                icd_code_list[row["SEQ_NUM"] - 1] = row["ICD9_CODE"]
+            icd_code_list = [icd for icd in icd_code_list if icd is not None]
+            ndf = pd.DataFrame(
+                data={
+                    "HADM_ID": [df["HADM_ID"].iloc[0]],
+                    "ICD9_CODE_LIST": ["/".join(icd_code_list)]
+                },
+            )
+            return ndf
+
+        print("Loading and processing MIMIC ICD diagnosis data ...")
+
+        diag_data = pd.read_csv(mimic_diag_file)
+        diag_data = diag_data[["HADM_ID", "SEQ_NUM", "ICD9_CODE"]]
+        diag_data = diag_data.dropna()
+        diag_data["HADM_ID"] = diag_data["HADM_ID"].astype(str, copy=False)
+        diag_data["ICD9_CODE"] = diag_data["ICD9_CODE"].astype(str, copy=False)
+        diag_data["SEQ_NUM"] = diag_data["SEQ_NUM"].astype(int, copy=False)
+        diag_data = diag_data.groupby("HADM_ID").apply(_merge_icd).reset_index(drop=True)
+        diag_data = diag_data.set_index("HADM_ID")
+        # print(diag_data)
+
+        diag_data.to_csv(mimic_diag_file + "_processed.csv")
+
+        result = pd.merge(diag_data, note_data, on='HADM_ID')
+        # print(result)
+
+        result.to_csv(mimic_file)
+        print("MIMIC note data and ICD diagnosis data have been merged and saved to %s" % mimic_file)
+
+    else:
+        print("Loading MIMIC note data and ICD diagnosis data from %s" % mimic_file)
+        result = pd.read_csv(mimic_file)
+
+    return result
+
+if __name__ == "__main__":
+    # load_hpo_ontology()
+    # plot_hpo_ontology(data)
+    load_hpo_description()
+    # load_mimic()
---
+++ src/pretrain.py
@@ -0,0 +1,12 @@
+import torch
+from pytorch_pretrained_bert import BertForPreTraining
+
+
+
+def main():
+    model = BertForPreTraining.from_pretrained()
+
+    pass
+
+if __name__ == '__main__':
+    main()
